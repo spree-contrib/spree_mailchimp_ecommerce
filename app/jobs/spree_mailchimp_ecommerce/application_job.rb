054e370e8528ff5d496807b35225c3e82e683143
Oleg Leontiev <oleg.leontiev@sparksolutions.co>
2019-02-01 12:14:29 +0100

    remove duplication

--- app/jobs/spree_mailchimp_ecommerce/application_job.rb
+++ app/jobs/spree_mailchimp_ecommerce/application_job.rb
@@ -1,4 +1,9 @@
 module SpreeMailchimpEcommerce
   class ApplicationJob < ActiveJob::Base
+    private
+    def gibbon_store
+      Gibbon::Request.new(api_key: ::SpreeMailchimpEcommerce.configuration.mailchimp_api_key).
+        ecommerce.stores(::SpreeMailchimpEcommerce.configuration.mailchimp_store_id)
+    end
   end
 end
--- app/jobs/spree_mailchimp_ecommerce/update_user_job.rb
+++ app/jobs/spree_mailchimp_ecommerce/update_user_job.rb
@@ -2,11 +2,7 @@ module SpreeMailchimpEcommerce
   class UpdateUserJob < ApplicationJob
     def perform(user)
       return unless user.mailchimp_user
-
-      Gibbon::Request.new(api_key: ::SpreeMailchimpEcommerce.configuration.mailchimp_api_key).ecommerce.
-        stores(::SpreeMailchimpEcommerce.configuration.mailchimp_store_id).
-        customers(user.mailchimp_user["id"]).
-        update(body: user.reload.mailchimp_user)
+      gibbon_store.customers(user.mailchimp_user["id"]).update(body: user.reload.mailchimp_user)
     end
   end
 end

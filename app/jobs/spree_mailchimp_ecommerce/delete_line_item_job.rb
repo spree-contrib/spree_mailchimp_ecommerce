ed8196fbf2fd98b5c9d9200e4360d8a527c30ddb
Oleg Leontiev <oleg.leontiev@sparksolutions.co>
2019-06-04 13:28:19 +0200

    tweaks

--- app/jobs/spree_mailchimp_ecommerce/delete_line_item_job.rb
+++ app/jobs/spree_mailchimp_ecommerce/delete_line_item_job.rb
@@ -6,6 +6,8 @@ module SpreeMailchimpEcommerce
       line = ::Spree::LineItem.find(line_id)
 
       gibbon_store.carts(line.order.number).lines(Digest::MD5.hexdigest("#{line.id}#{line.order_id}")).delete
+    rescue Gibbon::MailChimpError => e
+      Rails.logger.warn "[MAILCHIMP] Failed to delete line item = #{line.id}. #{e}"
     end
   end
 end

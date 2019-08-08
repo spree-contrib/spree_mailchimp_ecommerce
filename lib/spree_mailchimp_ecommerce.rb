14c84e539f869418281716faf7a792d4ab88efb3
Oleg Leontiev <oleg.leontiev@sparksolutions.co>
2019-08-08 13:45:10 +0200

    use env

--- lib/spree_mailchimp_ecommerce.rb
+++ lib/spree_mailchimp_ecommerce.rb
@@ -5,24 +5,15 @@ require "gibbon"
 
 module SpreeMailchimpEcommerce
   class << self
-    def configuration
-      Configuration.new
-    end
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
   end
 
   class Configuration
-    ATTR_LIST = [:mailchimp_api_key, :mailchimp_store_id, :mailchimp_list_id, :mailchimp_store_name, :cart_url]
-
-    ATTR_LIST.each do |a|
-      define_method a do
-        setting_model.try(a)
-      end
-    end
-
-    private
-
-    def setting_model
-      ::MailchimpSetting.last
-    end
+    attr_accessor :mailchimp_api_key, :mailchimp_store_id, :mailchimp_list_id, :mailchimp_store_name, :cart_url
   end
 end

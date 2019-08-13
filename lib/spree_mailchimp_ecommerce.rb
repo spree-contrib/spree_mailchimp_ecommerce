a410550e0b5476cfd1d0f4a2decdb025f74b6cc3
Oleg Leontiev <oleg.leontiev@sparksolutions.co>
2019-08-13 11:03:01 +0200

    Revert "Merge pull request #55 from spark-solutions/env_settings"
    This reverts commit c709f90c6795d36d07fb5b83229e9741d8a61a19, reversing
    changes made to 181c93c5f789d1b9836a0c2e88c411bad06d6091.

--- lib/spree_mailchimp_ecommerce.rb
+++ lib/spree_mailchimp_ecommerce.rb
@@ -5,15 +5,24 @@ require "gibbon"
 
 module SpreeMailchimpEcommerce
   class << self
-    attr_accessor :configuration
-  end
-
-  def self.configure
-    self.configuration ||= Configuration.new
-    yield(configuration)
+    def configuration
+      Configuration.new
+    end
   end
 
   class Configuration
-    attr_accessor :mailchimp_api_key, :mailchimp_store_id, :mailchimp_list_id, :mailchimp_store_name, :cart_url
+    ATTR_LIST = [:mailchimp_api_key, :mailchimp_store_id, :mailchimp_list_id, :mailchimp_store_name, :cart_url]
+
+    ATTR_LIST.each do |a|
+      define_method a do
+        setting_model.try(a)
+      end
+    end
+
+    private
+
+    def setting_model
+      ::MailchimpSetting.last
+    end
   end
 end

442fc19eeb9678a71bad9efd1a752450c76949e9
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-08-14 07:40:31 +0200

    Corrects order_spec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,7 +1,6 @@
 require "spec_helper"
 
 describe Spree::Order, type: :model do
-  subject { build(:order) }
   before { allow_any_instance_of(SpreeMailchimpEcommerce::Configuration).to receive(:cart_url) { "test.com/cart" } }
   before { allow(SpreeMailchimpEcommerce).to receive(:configuration).and_return(SpreeMailchimpEcommerce::Configuration.new) }
   describe "json" do

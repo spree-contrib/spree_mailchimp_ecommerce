f04d1276af87bd650c450074a09660b404ff3f86
Damian Legawiec <damian@sparksolutions.co>
2019-09-20 11:15:58 +0200

    Fix migrations for older Spree versions

--- db/migrate/20190910090133_add_account_name_to_mailchimp_setting.rb
+++ db/migrate/20190910090133_add_account_name_to_mailchimp_setting.rb
@@ -1,4 +1,4 @@
-class AddAccountNameToMailchimpSetting < ActiveRecord::Migration[5.2]
+class AddAccountNameToMailchimpSetting < SpreeExtension::Migration[4.2]
   def change
     add_column :mailchimp_settings, :mailchimp_account_name, :string
   end

d49c5d6d20e7950771ffb244afdd2721bf48993d
Oleg Leontiev <youzik@MacBook-Pro-Oleg-2.local>
2019-09-20 14:30:26 +0200

    add ensure block to sync job

--- app/jobs/spree_mailchimp_ecommerce/upload_store_content_job.rb
+++ app/jobs/spree_mailchimp_ecommerce/upload_store_content_job.rb
@@ -2,28 +2,28 @@ module SpreeMailchimpEcommerce
   class UploadStoreContentJob < ApplicationJob
     def perform(*_args)
       begin
-      gibbon_store.update(body: { is_syncing: true })
+        gibbon_store.update(body: { is_syncing: true })
 
-      ::Spree::Product.find_each do |product|
-        ::SpreeMailchimpEcommerce::CreateProductJob.perform_now(product.mailchimp_product)
-      end
+        ::Spree::Product.find_each do |product|
+          ::SpreeMailchimpEcommerce::CreateProductJob.perform_now(product.mailchimp_product)
+        end
 
-      ::Spree::User.where.not(email: nil).find_each do |user|
-        ::SpreeMailchimpEcommerce::CreateUserJob.perform_now(user.mailchimp_user)
-      end
+        ::Spree::User.where.not(email: nil).find_each do |user|
+          ::SpreeMailchimpEcommerce::CreateUserJob.perform_now(user.mailchimp_user)
+        end
 
-      ::Spree::Order.complete.find_each do |order|
-        ::SpreeMailchimpEcommerce::CreateOrderJob.perform_now(order.mailchimp_order)
-      end
+        ::Spree::Order.complete.find_each do |order|
+          ::SpreeMailchimpEcommerce::CreateOrderJob.perform_now(order.mailchimp_order)
+        end
 
-      ::Spree::Promotion.find_each do |promotion|
-        ::SpreeMailchimpEcommerce::CreatePromoRuleJob.perform_now(promotion.mailchimp_promo_rule)
-        ::SpreeMailchimpEcommerce::CreatePromoCodeJob.perform_now(promotion.mailchimp_promo_rule, promotion.mailchimp_promo_code)
-      end
-
-      gibbon_store.update(body: { is_syncing: false })
+        ::Spree::Promotion.find_each do |promotion|
+          ::SpreeMailchimpEcommerce::CreatePromoRuleJob.perform_now(promotion.mailchimp_promo_rule)
+          ::SpreeMailchimpEcommerce::CreatePromoCodeJob.perform_now(promotion.mailchimp_promo_rule, promotion.mailchimp_promo_code)
+        end
       rescue Gibbon::MailChimpError => e
         Rails.logger.error("[MAILCHIMP] Error while syncing process: #{e}")
+      ensure
+        gibbon_store.update(body: { is_syncing: false })
       end
     end
   end

af871c18639eadf98693649efc8c0a05fe2c77ce
Oleg Leontiev <oleg.leontiev@sparksolutions.co>
2019-09-23 14:29:21 +0200

    use spree migration

--- db/migrate/20190917095127_add_state_to_settings.rb
+++ db/migrate/20190917095127_add_state_to_settings.rb
@@ -1,4 +1,4 @@
-class AddStateToSettings < ActiveRecord::Migration[5.2]
+class AddStateToSettings < SpreeExtension::Migration[4.2]
   def change
     add_column :mailchimp_setting, :state, :string, default: 'inactive'
   end

2fa65344a5052eb80f643ff6554b65c2baea7958
Oleg Leontiev <oleg.leontiev@sparksolutions.co>
2019-09-30 16:25:00 +0200

    add rescue
    return state

--- app/jobs/spree_mailchimp_ecommerce/delete_store_job.rb
+++ app/jobs/spree_mailchimp_ecommerce/delete_store_job.rb
@@ -1,7 +1,11 @@
 module SpreeMailchimpEcommerce
   class DeleteStoreJob < ApplicationJob
     def perform
-      gibbon_store.delete
+      begin
+        gibbon_store.delete
+      rescue Gibbon::MailChimpError => e
+        Rails.logger.warn "[MAILCHIMP] Failed to delete store. #{e}"
+      end
     end
   end
 end

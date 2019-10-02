9cf833e77920f668f44f6e885bbeedd9f90a593c
Oleg Leontiev <oleg.leontiev@sparksolutions.co>
2019-10-02 15:38:20 +0200

    move decorator for Zeitwerk support

--- lib/spree_mailchimp_ecommerce.rb
+++ lib/spree_mailchimp_ecommerce.rb
@@ -2,7 +2,7 @@ require "spree_core"
 require "spree_extension"
 require "spree_mailchimp_ecommerce/engine"
 require "gibbon"
-require "gibbon_decorator"
+require "spree_mailchimp_ecommerce/gibbon_decorator"
 
 module SpreeMailchimpEcommerce
   class << self

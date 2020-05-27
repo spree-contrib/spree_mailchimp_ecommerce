515f9b3a9fc01a42692cc2474fba08dda4ecfd6f
Jordan Moncharmont <jormon@gmail.com>
2020-05-26 17:30:08 -0700

    manually do an upsert

--- app/jobs/spree_mailchimp_ecommerce/upsert_order_job.rb
+++ app/jobs/spree_mailchimp_ecommerce/upsert_order_job.rb
@@ -3,7 +3,14 @@
 module SpreeMailchimpEcommerce
   class UpsertOrderJob < ApplicationJob
     def perform(mailchimp_order)
-      gibbon_store.orders(mailchimp_order["id"]).upsert(body: mailchimp_order)
+      gibbon_store.orders(mailchimp_order["id"]).update(body: mailchimp_order)
+    rescue Gibbon::MailChimpError => e
+      if e.status_code == 404
+        gibbon_store.orders(mailchimp_order["id"]).create(body: mailchimp_order)
+      else
+        # re-raise
+        raise
+      end
     end
   end
 end
